ed6ede7e36d0ddd3b96d7fe692cda540449c3565
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-20 23:35:17 +0100

    Created webpack enviroments and configurations

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,11 @@
 // console.log('Hello World!');
 
 console.log("Welcome to graphql server");
+
+// Hot Module Replacement
+
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => console.log('Module disposed. '));
+    
+}
