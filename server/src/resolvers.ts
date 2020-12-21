2b6b0cd01bb3bf1d9a3a890fc2daaa0807853774
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-21 02:20:40 +0100

    Apolloserver setup and working

---
+++ server/src/resolvers.ts
@@ -0,0 +1,14 @@
+
+export default {
+
+    Query: {
+        numberSixTwo() {
+            return 6 * 2;
+        },
+
+        serverName() {
+            return "The server name is sola's server";
+        }
+
+    },
+};
