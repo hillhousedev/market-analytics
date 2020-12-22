afc214777aa21abbaddeb695646209bb27a0718b
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-22 12:19:37 +0100

    Services added

---
+++ server/src/services/logger.ts
@@ -0,0 +1,20 @@
+import winston from 'winston'
+
+const logger = winston.createLogger({
+    format: winston.format.json(),
+    level: 'debug',
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error', maxFiles: 2, maxsize: 10000}),
+        new winston.transports.File({ filename: 'combine.log', maxFiles: 2, maxsize: 10000}),
+    ],
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
+    )
+}
+
+export default logger 
