29f4091aab393d9b6d17eb77d0e0ea30d1da1db6
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-23 11:26:24 +0100

    FX And Company graphql done

---
+++ server/src/graph-ql/fx/Fx.resolver.ts
@@ -0,0 +1,33 @@
+import { IResolvers } from 'graphql-tools'
+import FxService from './Fx.service';
+import { Container } from 'typedi';
+import { pubsub } from '../../pubsub';
+import logger from '../../services/logger';
+import { withCancel } from '../../utils/asyncIteratorUtils'
+
+const fxService = Container.get(FxService)
+
+const resolvers: IResolvers = {
+    Query: {
+        getPriceHistory:  async (_, args: {id: string }) => {
+            return await fxService.getPriceHistory(args.id)
+        },
+    },
+    Subscription: {
+        getFXPriceUpdates: {
+            subscribe: (_, args: { id: string}) => {
+                logger.debug(`Subscribe FX updates for ${args.id}`)
+
+                fxService.subscribePriceUpdates(args.id)
+                const result = pubsub.asyncIterator(`FX_CURRENT_PRICING.${args.id}`)
+
+                return withCancel(result, () => {
+                    logger.debug(`Unsubscribe FX updates for ${args.id}`)
+                    fxService.unsubscribePriceUpdates()
+                })
+            },
+        },
+    },
+}
+
+export default resolvers 
---
+++ server/src/graph-ql/fx/Fx.schema.ts
@@ -0,0 +1,39 @@
+import { gql } from 'apollo-server'
+
+export default gql`
+
+    type FxCurrencies {
+        code: ID!
+        name: String!
+
+    }
+
+    type FxSymbol {
+        id: String!
+    }
+
+    type FxPricing {
+        Pair: FxSymbol!
+        ask: Float!
+        bid: Float!
+        creationTimestamp: Float!
+        mid: Float!
+        valueDate: ISODateTime!
+    }
+
+    type FxRate {
+        Symbol: FxSymbol!
+        Bid: Float
+        Ask: Float
+        Mid: Float
+        ValueDate: ISODateTime
+        CreationTimestamp: Float
+
+    }
+
+    type FxSymbols {
+        currencies: [FxCurrencies!]!
+        pairs: [FxSymbol!]!
+    }
+
+`
